4ace35e9062b85651cec7fe4248bd336994e1e89
Raguram <raghuraam07@gmail.com>
2026-05-15 18:16:07 +0530

    Switch from InMemory to SQLite

--- TaskMgmt/Program.cs
+++ TaskMgmt/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TaskDb"));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=taskmanager.db"));
 builder.Services.AddScoped<ITaskService, TaskService>();
 
 builder.Services.AddCors(options =>
