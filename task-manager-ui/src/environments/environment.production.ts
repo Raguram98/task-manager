be9328d68eeb60615fc4e36bf2031acfc7e5350c
Raguram <raghuraam07@gmail.com>
2026-08-20 18:04:27 +0530

    Update production API URL

--- task-manager-ui/src/environments/environment.production.ts
+++ task-manager-ui/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://task-manager-backend-60mq.onrender.com/api'
+  apiUrl: 'https://task-manager-api-prod-bvgad3gkcfbud3fz.centralindia-01.azurewebsites.net/api'
 };
