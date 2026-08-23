c8a4e1e372e48ad26ab5f7059d0d227a9f6f6a45
Raguram <raghuraam07@gmail.com>
2026-08-23 13:40:35 +0530

    Add migration for DueDate column on Tasks table

--- TaskMgmt/Migrations/20260823080747_AddDueDateToTasks.Designer.cs
+++ TaskMgmt/Migrations/20260823080747_AddDueDateToTasks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskMgmt.Data;
 
@@ -11,9 +12,11 @@ using TaskMgmt.Data;
 namespace TaskMgmt.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260823080747_AddDueDateToTasks")]
+    partial class AddDueDateToTasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
